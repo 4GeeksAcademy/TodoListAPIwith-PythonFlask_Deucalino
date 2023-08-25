b2db8630154df2586ec25745f276659c9b6b8dfc
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-08-25 16:10:15 +0000

    learnpack tracked me back exercise3 and can not go  ahead even I reached ex 6

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+todos = [
+    { "label": "Don´t kiss Jenni Hermmosillo in the mouth when Spain wins the Feminine football Worldcup", "done": False },
+    { "label": "Don´t believe the hype..do..don´t..don´t...", "done": True }
+    ]
 
 app = Flask(__name__)
 @app.route('/todos', methods=['GET'])
 def hello():
-  return "<h1>Hello!</h1>" 
-
-
-
-
+  json_text=jsonify(todos)
+  return json_text
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
