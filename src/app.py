f8fecac9477201c5fc74de7fd58c3f51025b4c6f
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-08-27 15:33:27 +0000

    OK

--- src/app.py
+++ src/app.py
@@ -13,7 +13,11 @@ def add_new_todo():
     print(request_body)
     todos.append(request_body)
     return jsonify(todos)
-
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.remove(todos[position])
+    print("This is the position to delete: ",position)
+    return jsonify(todos)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
